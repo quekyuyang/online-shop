e5cab4c08ac31befa39910a9f20f9c6b9f0edea1
Yu Yang Quek <quekyuyang@gmail.com>
2022-05-28 07:56:52 +0200

    Validate server side that no more than 5 images were uploaded in product form

--- shop/forms.py
+++ shop/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Product
-
+from django.core.exceptions import ValidationError
 
 class ProductForm(forms.ModelForm):
     images = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple':True}))
@@ -9,6 +9,11 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ['name', 'quantity', 'price']
         widgets = {'name': forms.Textarea(attrs={'cols':80, 'rows':1})}
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if len(self.files.getlist('images')) > 5:
+            raise ValidationError('You can only upload up to 5 images')
 
 
 class AddToCartForm(forms.Form):
