188340f3e7dd50104d777e5269983047e31d0d19
Yu Yang Quek <quekyuyang@gmail.com>
2022-05-31 21:20:10 +0200

    Move some common test setup procedures to setup and teardown methods

--- shop/tests.py
+++ shop/tests.py
@@ -9,16 +9,20 @@ import shutil
 
 @override_settings(MEDIA_ROOT='test_media/')
 class AddProductViewTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        dirpath = Path(__file__).parent / 'test_images'
+        cls.images = []
+        for path in list(dirpath.iterdir()):
+            cls.images.append(open(path, 'rb'))
+
     def setUp(self):
         user = User.objects.create_user('user1', password='misoramen1')
         self.client.force_login(user)
 
     def test_add_product(self):
-        dirpath = Path(__file__).parent / 'test_images'
-        images = []
-        for path in list(dirpath.iterdir())[:5]:
-            images.append(open(path, 'rb'))
-
+        images = self.images[:5]
         form_data = dummy_product_form_data(images)
         response = self.client.post(reverse('add_product'), form_data)
         self.assertRedirects(response, reverse('homepage'))
@@ -30,28 +34,24 @@ class AddProductViewTests(TestCase):
         self.assertEqual(len(product.productimage_set.all()), len(images))
 
         i_primary_image = form_data['i_primary_image']
-        images[i_primary_image].seek(0)
+        self.images[i_primary_image].seek(0)
         self.assertEqual(product.primary_image.image.read(), images[i_primary_image].read())
 
         product.primary_image.image.close()
-        for image in images:
-            image.close()
         shutil.rmtree('test_media/')
 
     def test_too_many_images(self):
-        dirpath = Path(__file__).parent / 'test_images'
-        images = []
-        image_paths = list(dirpath.iterdir())
-        self.assertGreater(len(image_paths), 5)
-        for path in image_paths:
-            images.append(open(path, 'rb'))
+        self.assertGreater(len(self.images), 5)
 
-        form_data = dummy_product_form_data(images)
+        form_data = dummy_product_form_data(self.images)
         response = self.client.post(reverse('add_product'), form_data)
         self.assertEqual(response.templates[0].name, 'shop/add_product.html')
         self.assertEqual(len(Product.objects.all()), 0)
 
-        for image in images:
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        for image in cls.images:
             image.close()
 
 
