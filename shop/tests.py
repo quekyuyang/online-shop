814b1199b813b53b3d439ded7ef2dccc114b2375
Yu Yang Quek <quekyuyang@gmail.com>
2022-06-04 11:18:53 +0200

    Test login view

--- shop/tests.py
+++ shop/tests.py
@@ -145,9 +145,29 @@ class CartTest(TestCase):
             'price': 45.69,
         }
         session.save()
-        
+
         response = self.client.post(reverse('remove_from_cart', args=[product.id]))
         session = self.client.session
         cart = session[settings.CART_SESSION_ID]
         self.assertRedirects(response, reverse('cart'))
         self.assertEqual(cart, {})
+
+
+class LoginTest(TestCase):
+    def test_login_view(self):
+        response = self.client.get(reverse('login'))
+        form = response.context['form']
+        self.assertEqual(response.templates[0].name, 'shop/login.html')
+        self.assertEqual(len(form.data), 0)
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
+
+        user = User.objects.create_user('user1', password='misoramen1')
+        form_data = {
+            'username': 'user1',
+            'password': 'misoramen1'
+        }
+
+        response = self.client.post(reverse('login'), form_data)
+        self.assertRedirects(response, reverse('homepage'))
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+        self.assertEqual(response.wsgi_request.user, user)
