952b69c389f6d6fb4b2f321612d46f3dd51a3c3c
Yu Yang Quek <quekyuyang@gmail.com>
2022-06-09 20:54:11 +0200

    Move primary image index validation from form's clean to field's clean

--- shop/forms.py
+++ shop/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Product
 from django.core.exceptions import ValidationError
 
+
 class ProductForm(forms.ModelForm):
     images = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple':True}))
     i_primary_image = forms.IntegerField()
@@ -13,7 +14,7 @@ class ProductForm(forms.ModelForm):
 
     def clean_i_primary_image(self):
         i_primary_image = self.cleaned_data.get('i_primary_image')
-        if i_primary_image < 0:
+        if i_primary_image < 0 or i_primary_image > len(self.files.getlist('images')) - 1:
             raise ValidationError('Invalid primary image index')
         return i_primary_image
 
@@ -21,10 +22,6 @@ class ProductForm(forms.ModelForm):
         cleaned_data = super().clean()
         if len(self.files.getlist('images')) > 5:
             raise ValidationError('You can only upload up to 5 images')
-
-        i_primary_image = cleaned_data.get('i_primary_image')
-        if i_primary_image and i_primary_image > len(self.files.getlist('images')) - 1:
-            raise ValidationError('Primary image index exceeding number of images')
 
 
 class AddToCartForm(forms.Form):
