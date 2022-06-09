1419e6dbe50952227f4477e809976ec402a55fb4
Yu Yang Quek <quekyuyang@gmail.com>
2022-06-09 20:49:57 +0200

    Validate that primary image index is not negative in product form

--- shop/forms.py
+++ shop/forms.py
@@ -11,12 +11,19 @@ class ProductForm(forms.ModelForm):
         fields = ['name', 'quantity', 'price', 'categories']
         widgets = {'name': forms.Textarea(attrs={'cols':80, 'rows':1})}
 
+    def clean_i_primary_image(self):
+        i_primary_image = self.cleaned_data.get('i_primary_image')
+        if i_primary_image < 0:
+            raise ValidationError('Invalid primary image index')
+        return i_primary_image
+
     def clean(self):
         cleaned_data = super().clean()
         if len(self.files.getlist('images')) > 5:
             raise ValidationError('You can only upload up to 5 images')
 
-        if cleaned_data['i_primary_image'] > len(self.files.getlist('images')) - 1:
+        i_primary_image = cleaned_data.get('i_primary_image')
+        if i_primary_image and i_primary_image > len(self.files.getlist('images')) - 1:
             raise ValidationError('Primary image index exceeding number of images')
 
 
